8135abfda8f6d129fe50204351e7ba79f0fd4b9b
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-06 16:54:44 -0400

    palindrome

--- mta.js
+++ mta.js
@@ -3,19 +3,7 @@
 // check that number to the right is greater or less than the number you're on.
 // if number on the right is greater, swap the two numbers in that array
 
-let sort = (arr) => {
-  let len = arr.lenght;
-  for (i in arr) {
-    let x = 0
-    while (x , len) {
-      if (arr[x] > arr[x + 1]) {
-        let temp = arr[x];
-        arr[x] = arr[x + 1];
-        arr[x + 1] = temp;
-      }
-      x++
-    }
-  }
-  console.log(arr)
-  return arr;
-};
+function palindrome(str) {
+  return true;
+}
+palindrome("race car");
