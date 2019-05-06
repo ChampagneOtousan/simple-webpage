511df63a890db65a821efc71a4138b9cbbe7baf6
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-06 10:59:05 -0400

    added while loop

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 console.log("Hello World")
 
-function sayHello() {
-  document.write("Hello User")
+let i = 0
+
+while (i < 30) {
+  console.log(i + 1)
+  i += 1
 }
 
-sayHello()
 
-let x1 = document.getElementsByClassName("header1")[0]
-console.log(x1)
 
-let x2 = document.getElementsByTagName("h1")[0]
-console.log(x2)
+// Different methods of selection
 
-let x3 = document.getElementById("header1")
-console.log(x3)
-
-let x4 = document.querySelector("#header1")
-console.log(x4)
+// let x1 = document.getElementsByClassName("header1")[0]
+// console.log(x1)
+//
+// let x2 = document.getElementsByTagName("h1")[0]
+// console.log(x2)
+//
+// let x3 = document.getElementById("header1")
+// console.log(x3)
+//
+// let x4 = document.querySelector("#header1")
+// console.log(x4)
