f049b632160a3a229d9c7b84a22f32ee63ff04b5
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-06 14:31:15 -0400

    simplified

--- mta.js
+++ mta.js
@@ -1,45 +1,32 @@
-let balance = 100
 let fare = 2.75
-Atrain = []
-
+let train = []
 
 class MetroCard { // define a class like a function, represents an object
-    constructor(amt) {
-      this.amt = amt
+  constructor(amt) {
+    this.amt = amt
   }
 }
 
-getCard = () => {
+function getCard() {
   let amount = prompt("How much would you like to add?")
-  balance -= (amount - 1)
-  let card = new MetroCard(parseInt(amount))
-    return card
+  parseInt(amount)
+
 }
 
 newCard = getCard()
 
-swipe = (card) => {
-  console.log(fare)
-  console.log(card.amt)
-  if (card.amt < fare) {
+swipe = () => {
+  if (card < fare) {
     return 'INSUFFICIENT FARE'
-} else if (card.amt >= fare) {
-  card.amt -= fare
-  Atrain.push(card)
-  console.log(card.amt)
+  } else if (card >= fare) {
+    card -= fare
+    train.push(person)
     return 'GO'
   }
 }
 
-
-
-
-
-// get a metrocard
- --> load money
-
- // every ride = 2.75
-
- // swipe your metrocard
-
- // go through turnstile
+// create card variable
+// swupe the card
+// check to see if the card has enoughf fare
+// if yes, get on the train
+// if no, refill card
